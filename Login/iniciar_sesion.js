6b5201abeb39bcaecc6630c91f0acfcf040a9216
Elias Esperanza <eliasesperanza261380@gmail.com>
2024-06-10 14:59:47 -0600

    URL API

--- Login/iniciar_sesion.js
+++ Login/iniciar_sesion.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         try {
-            const response = await fetch('http://localhost:3000/login', {
+            const response = await fetch('https://backend-points-production.up.railway.app/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
