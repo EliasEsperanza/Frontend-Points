0213ad524e8c7e5a950011782f56e2a44a017a18
Hieloeston235 <gabrieldelao235@gmail.com>
2024-06-17 23:18:33 -0600

    Crear premio y nivel listo

--- RamitaPrincipal/js/crear_niveles.js
+++ RamitaPrincipal/js/crear_niveles.js
@@ -1,32 +1,31 @@
+const descripcion = document.getElementById("NombreNivel");
+const puntosInicio = document.getElementById("PuntosInicio");
+const puntosFin = document.getElementById("PuntosFin");
+const icono = document.getElementById('adjunto');
 const form_niveles = document.getElementById("form-Nivel");
-    form_niveles.addEventListener('submit', function(event){
-        event.preventDefault();
-        try {
+form_niveles.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-            const descripcion = document.getElementById("NombreNivel").textContent;
-            const puntosInicio = document.getElementById("PuntosInicio").textContent;
-            const puntosFin = document.getElementById("PuntosFin").textContent;
-            const icono = document.getElementById("adjunto").files[0];
-            //ingresa el nuevo nivel
-            if(icono){
-                console.log('Hay icono');
-            }
-            /*const response = await fetch(`https://backend-points-production.up.railway.app/niveles`,{
-                method:'POST',
-                body: JSON.stringify({
-                    descripcion:descripcion,
-                    puntosInicio:puntosInicio,
-                    puntosFin:puntosFin,
-                    icono:icono
-                })
-            });*/
-        } catch (error) {
-            console.error('Error:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Error al enviar nivel',
-                text: `Ha ocurrido un error .Detalle: ${error.message}`
-            });
+
+    const formData = new FormData();
+    formData.append('descripcion', descripcion.value);
+    formData.append('puntosInicio', puntosInicio.value);
+    formData.append('puntosFin', puntosFin.value)
+    formData.append('icono', icono.files[0]);
+
+    axios.post('https://backend-points-production.up.railway.app/niveles', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
         }
-    });
+    })
+        .then(response => {
+            console.log('Nivel creado exitosamente:', response.data);
+            alert('Nivel creado exitosamente');
 
+            formPremio.reset();
+        })
+        .catch(error => {
+            console.error('Error al crear el nivel:', error);
+            alert('Error al crear el Nivel');
+        });
+});
